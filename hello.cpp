21a403628f674ef05f5fabf20ed5e8af8063105f
C.y <yangruihan@vip.qq.com>
2018-04-28 23:50:15 +0800

    include subdirectory

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
 #include "TutorialConfig.h"
+#include "include/mmath.h"
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    std::cout 
+    << "Hello World" << std::endl;
     std::cout << "Major version number: " << Tutorial_VERSION_MAJOR << std::endl;
     std::cout << "Minor version number: " << Tutorial_VERSION_MINOR << std::endl;
+
+#ifdef USE_MMATH
+    std::cout << "1 + 2 = " << mmath::add(1, 2) << std::endl;
+#endif
+
     return 0;
 }
---
+++ include/mmath.h
@@ -0,0 +1,10 @@
+#ifndef MMATH_H
+#define MMATH_H
+
+class mmath
+{
+public:
+    static int add(const int& a, const int&b);
+};
+
+#endif // !MMATH_H
