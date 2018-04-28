21c581b87110b2bb3c81c6a5a721aa8026dc05ae
C.y <yangruihan@vip.qq.com>
2018-04-28 23:20:42 +0800

    add version number && configure file

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+#include "TutorialConfig.h"
+
 int main()
 {
     std::cout << "Hello World" << std::endl;
-
+    std::cout << "Major version number: " << Tutorial_VERSION_MAJOR << std::endl;
+    std::cout << "Minor version number: " << Tutorial_VERSION_MINOR << std::endl;
     return 0;
 }
